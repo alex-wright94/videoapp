0097b94755de42ba71bd0692917e3171f85c9cbf
Alex Wright <alexwright@Alexs-MacBook-Pro.local>
2020-05-21 13:38:42 -0400

    initial xcode project creation

---
+++ videoapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  videoapp
+//
+//  Created by Alex Wright on 5/21/20.
+//  Copyright © 2020 Alex Wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
