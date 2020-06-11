7ab12b7fefe147b27c3d620752d2826950049fc2
Alex Wright <alexwright@Alexs-MacBook-Pro.local>
2020-06-11 15:28:45 -0400

    changed gitignore to stop tracking constants and added constants template

---
+++ videoapp/ConstantsTemplate.swift
@@ -0,0 +1,21 @@
+//
+//  ConstantsTemplate.swift
+//  videoapp
+//
+//  Created by Alex Wright on 6/11/20.
+//  Copyright © 2020 Alex Wright. All rights reserved.
+//
+//add your api keys and uncomment the code
+import Foundation
+
+
+
+
+//struct Constants {
+//
+//    static var API_KEY = ""
+//    static var PLAYLIST_ID = ""
+//    static var API_URL = "  'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=25&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+//
+//}
+//
